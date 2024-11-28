dd0db31945fca20b51c7ad0c826ec8467daca75f
Néstor de Dios <nestordedios@gmail.com>
2024-11-28 16:34:26 -0300

    react scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -122,6 +122,10 @@ async function loadEager(doc) {
   } catch (e) {
     // do nothing
   }
+
+  loadHeader(doc.querySelector('header'));
+  loadFooter(doc.querySelector('footer'));
+
   //import('./cme/jquery.1fc733054ddb2e2fcd4a0f763671047b.js');
   cmeScripts();
 }
@@ -208,12 +212,16 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
-
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
 
+  import('./cme/custom-navbar.870ed14da87b882b0c1f783292f3f12b.js');
+  import('./cme/content-toggle.785ccebfcf3031805ea88b853c6eb377.js');
+  import('./cme/collapsible.253c0ec6a3ac417570a8044eb4b0a38b.js');
+  import('./cme/feedback-form.9d433f3d15a7e2d07ac4f09d1d6cafa8.js');
+  import('./cme/language-selector.b2ced5c1f9ac40f8e7a1fefe30bde5a8.js');
+  import('./cme/vendor.c50054cf072d88684ad63be26c943c16.js');
+  import('./cme/common.52ce1b7dcd776b2ecc275fca40301c87.js');
   //import('./cme/dependencies.e420e99671686097433a87b2a109856a.js');
   cmeLazyScripts();
 }
